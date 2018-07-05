b82acfd1dd7aef02143f7a83e4c65a71bd5db8d0
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-07-05 17:32:26 +0200

    ajout collab et liste simple

---
+++ src/main/java/dev/sgp/entite/Collaborateur.java
@@ -0,0 +1,196 @@
+package dev.sgp.entite;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+public class Collaborateur {
+	private String matricule;
+	private String nom;
+	private String prenom;
+	private LocalDate dateDeNaissance;
+	private String adresse;
+	private String numeroSecuriteSociale;
+	private String emailPro;
+	private String photo;
+	private ZonedDateTime dateHeureCreation;
+	private boolean actif;
+
+	/**
+	 * @param matricule
+	 * @param nom
+	 * @param prenom
+	 * @param dateDeNaissance
+	 * @param adresse
+	 * @param numeroSecuriteSociale
+	 * @param emailPro
+	 * @param photo
+	 * @param dateHeureCreation
+	 * @param actif
+	 */
+	public Collaborateur(String matricule, String nom, String prenom, LocalDate dateDeNaissance, String adresse,
+			String numeroSecuriteSociale, String emailPro, String photo, ZonedDateTime dateHeureCreation,
+			boolean actif) {
+		super();
+		this.matricule = matricule;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateDeNaissance = dateDeNaissance;
+		this.adresse = adresse;
+		this.numeroSecuriteSociale = numeroSecuriteSociale;
+		this.emailPro = emailPro;
+		this.photo = photo;
+		this.dateHeureCreation = dateHeureCreation;
+		this.actif = actif;
+	}
+
+	/**
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+
+	/**
+	 * @param matricule
+	 *            the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom
+	 *            the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom
+	 *            the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the dateDeNaissance
+	 */
+	public LocalDate getDateDeNaissance() {
+		return dateDeNaissance;
+	}
+
+	/**
+	 * @param dateDeNaissance
+	 *            the dateDeNaissance to set
+	 */
+	public void setDateDeNaissance(LocalDate dateDeNaissance) {
+		this.dateDeNaissance = dateDeNaissance;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+	/**
+	 * @param adresse
+	 *            the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	/**
+	 * @return the numeroSecuriteSociale
+	 */
+	public String getNumeroSecuriteSociale() {
+		return numeroSecuriteSociale;
+	}
+
+	/**
+	 * @param numeroSecuriteSociale
+	 *            the numeroSecuriteSociale to set
+	 */
+	public void setNumeroSecuriteSociale(String numeroSecuriteSociale) {
+		this.numeroSecuriteSociale = numeroSecuriteSociale;
+	}
+
+	/**
+	 * @return the emailPro
+	 */
+	public String getEmailPro() {
+		return emailPro;
+	}
+
+	/**
+	 * @param emailPro
+	 *            the emailPro to set
+	 */
+	public void setEmailPro(String emailPro) {
+		this.emailPro = emailPro;
+	}
+
+	/**
+	 * @return the photo
+	 */
+	public String getPhoto() {
+		return photo;
+	}
+
+	/**
+	 * @param photo
+	 *            the photo to set
+	 */
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	/**
+	 * @return the dateHeureCreation
+	 */
+	public ZonedDateTime getDateHeureCreation() {
+		return dateHeureCreation;
+	}
+
+	/**
+	 * @param dateHeureCreation
+	 *            the dateHeureCreation to set
+	 */
+	public void setDateHeureCreation(ZonedDateTime dateHeureCreation) {
+		this.dateHeureCreation = dateHeureCreation;
+	}
+
+	/**
+	 * @return the actif
+	 */
+	public boolean isActif() {
+		return actif;
+	}
+
+	/**
+	 * @param actif
+	 *            the actif to set
+	 */
+	public void setActif(boolean actif) {
+		this.actif = actif;
+	}
+
+}
--- src/main/webapp/WEB-INF/views/collab/listerCollaborateurs.jsp
+++ src/main/webapp/WEB-INF/views/collab/listerCollaborateurs.jsp
@@ -1,3 +1,5 @@
+<%@page import="javax.swing.ListCellRenderer"%>
+<%@page import="dev.sgp.entite.Collaborateur"%>
 <%@ page import="java.util.List"%>
 <%@ page language="java" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -5,18 +7,25 @@
 <head>
 <meta charset="UTF-8">
 <title>SGP - App</title>
+<link rel="stylesheet"
+	href="<%=request.getContextPath()%>/bootstrap-4.1.1-dist/css/bootstrap.css">
 </head>
 <body>
 	<h1>Les collaborateurs</h1>
 	<ul>
 		<%
-			List<String> listeNoms = (List<String>) request.getAttribute("listeNoms");
-			for (String nom : listeNoms) {
+			List<Collaborateur> listeCollaborateurs = (List<Collaborateur>) request.getAttribute("listeCollaborateurs");
+			for (Collaborateur collab : listeCollaborateurs) {
 		%>
-		<li><%=nom%></li>
+		<li><%=collab.getMatricule()%></li>
 		<%
 			}
 		%>
 	</ul>
+	
+	<!-- jQuery first, then poper, then Bootstrap JS. -->
+	<script src="js/jquery-slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
