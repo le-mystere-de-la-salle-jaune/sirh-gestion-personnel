d719bbd5437c663dc53cde71e40734ae3a8ddd19
Corentin Torres <torres.corentin@gmail.com>
2018-07-05 14:52:01 +0200

    JSP (première page)

--- src/main/java/dev/sgp/web/EditerCollaborateurController.java
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -1,9 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
