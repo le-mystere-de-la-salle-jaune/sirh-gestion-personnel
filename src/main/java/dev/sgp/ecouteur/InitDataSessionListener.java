8100db5506bf3008883c6bfb4e93f3cfd5c946e6
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-07-06 16:45:26 +0200

    fix Listener | ServletContextListener au lieu de HttpSessionListener

--- src/main/java/dev/sgp/ecouteur/InitDataSessionListener.java
+++ src/main/java/dev/sgp/ecouteur/InitDataSessionListener.java
@@ -3,20 +3,20 @@ package dev.sgp.ecouteur;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
 
 import dev.sgp.entite.Collaborateur;
 import dev.sgp.service.CollaborateurService;
 import dev.sgp.util.Constantes;
 
 @WebListener
-public class InitDataSessionListener implements HttpSessionListener {
+public class InitDataSessionListener implements ServletContextListener {
 	private static final CollaborateurService collaborateurService = Constantes.COLLAB_SERVICE;
 
 	@Override
-	public void sessionCreated(HttpSessionEvent event) {
+	public void contextInitialized(ServletContextEvent event) {
 		collaborateurService.sauvegarderCollaborateur(new Collaborateur("M01", "Abitbol", "Georges", LocalDate.now(),
 				"2 rue Test", "123456789AZERTY", "mondedemerde@classe.com", "photo", ZonedDateTime.now(), true));
 		collaborateurService.sauvegarderCollaborateur(new Collaborateur("M02", "Cena", "John", LocalDate.now(),
@@ -24,10 +24,11 @@ public class InitDataSessionListener implements HttpSessionListener {
 		collaborateurService.sauvegarderCollaborateur(new Collaborateur("M03", "Pouilly-Choisol de Rochambeau-Polony",
 				"Jacques-Henri", LocalDate.now(), "Domaine Saint-Antoine de Cumont 24620", "999999995999999",
 				"darksasuke666@caramail.be", "photo", ZonedDateTime.now(), true));
+
 	}
 
 	@Override
-	public void sessionDestroyed(HttpSessionEvent arg0) {
-	}
+	public void contextDestroyed(ServletContextEvent arg0) {
 
+	}
 }
